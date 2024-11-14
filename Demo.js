697e9be820c68e8f550f1be7fd8905d19a6dd371
Shraddha Satish Mankar <vaishr9805@gmail.com>
2024-11-14 19:46:07 +0530

    Add files via upload

---
+++ Demo.js
@@ -0,0 +1,23 @@
+console.log("welcome to satish it");
+console.log(10+20);//no-no=addition
+console.log("10"+"20");//string-string=concat
+console.log(10+"20"); //no or  string=no and string print
+console.log(10 +"20"+10);//no or string or no=no string(no) no
+console.log(10+ +"10"+ +"10");//here is space and + oprator are double so that time increment
+console.log(10+ "10"+ +10);
+console.log(10-2);//sustract=8
+//only + symobol are concat to each other mean connect '-','*' are not this perform opration
+console.log(10-"2");//op=
+console.log(10+"2"-"2");//10 and 2 concat each othe and then substraction perform
+console.log(100+ +"100"-"100"*"100");//first priority goes to multiplication then minus and concat
+
+console.log(10+"20"+"30");
+console.log(10-20-"40");
+console.log(100*"100");
+console.log(90<80);
+console.log("80"/"10");
+console.log(40-"50");
+console.log(89-78+123);
+console.log("300"-200);
+
+
---
+++ demo2.js
@@ -0,0 +1,9 @@
+console.log(10>9);// yes 10 is greater that 9
+console.log(10>9>8);
+console.log(67<89);
+console.log(10 * "10");
+console.log("10"*"10");
+console.log(100/"100");//qoefficeint
+console.log(100/"0");//infinity that mean run time error occur
+
+
